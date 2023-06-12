878d1ac696914493da1fb1eda250220520e4e34e
Arun <69082994+Arun9600@users.noreply.github.com>
2023-06-12 19:10:07 +0530

    unwanted figure bracket removed

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 var form = document.getElementById("postForm");
 form.addEventListener("submit", function (e) {
-  {
-    e.preventDefault();
-    let name = document.getElementById("name").value;
-    let text = document.getElementById("text").value;
-    let text_error = document.getElementById("text_error");
-    let name_error = document.getElementById("name_error");
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let text = document.getElementById("text").value;
+  let text_error = document.getElementById("text_error");
+  let name_error = document.getElementById("name_error");
 
-    if (name == "") {
-      name_error.innerHTML = "Name is required";
-      name_error.style.color = "red";
-    }
-    if (text == "") {
-      text_error.innerHTML = "Text is required";
-      text_error.style.color = "red";
-    }
-
-    if (name != "") {
-      name_error.innerHTML = "";
-    }
-    if (text != "") {
-      text_error.innerHTML = "";
-    }
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        title: name,
-        body: text,
-      }),
-      headers: {
-        "Content-type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        let op = document.getElementById("test");
-        let output = `The entered name is ${data.title} and text is ${data.body}`;
-        if (name == "") {
-          op.innerHTML = "";
-        } else if (text == "") {
-          op.innerHTML = "";
-        } else {
-          op.innerHTML = output;
-        }
-      });
-    form.reset();
+  if (name == "") {
+    name_error.innerHTML = "Name is required";
+    name_error.style.color = "red";
   }
+  if (text == "") {
+    text_error.innerHTML = "Text is required";
+    text_error.style.color = "red";
+  }
+
+  if (name != "") {
+    name_error.innerHTML = "";
+  }
+  if (text != "") {
+    text_error.innerHTML = "";
+  }
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: name,
+      body: text,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      let op = document.getElementById("test");
+      let output = `The entered name is ${data.title} and text is ${data.body}`;
+      if (name == "") {
+        op.innerHTML = "";
+      } else if (text == "") {
+        op.innerHTML = "";
+      } else {
+        op.innerHTML = output;
+      }
+    });
+  form.reset();
 });
